ca501e4f62526ac57d48fcc3243f4e155d745995
Leo Tamminen <lettam@utu.fi>
2023-01-17 20:45:52 +0200

    teht1.2 Content

--- osa1/teht1.2/src/App.js
+++ osa1/teht1.2/src/App.js
@@ -9,36 +9,44 @@ const App = () => {
 
   const Header = (props) => {
     return (
-      <>
+      <div>
         <h1>{props.course}</h1>
-      </>
+      </div>
+    )
+  }
+
+  const Part = (props) => {
+    return (
+      <div>
+        <p>{props.part1} {props.exercises1}</p>
+        <p>{props.part2} {props.exercises2}</p>
+        <p>{props.part3} {props.exercises3}</p>
+      </div>
     )
   }
 
   const Content = (props) => {
     return (
-      <>
-        <p>{props.part1} {props.exercises1}</p>
-        <p>{props.part2} {props.exercises2}</p>
-        <p>{props.part3} {props.exercises3}</p>
-      </>
+      <div>
+        <Part part1={part1} exercises1={exercises1}/>
+        <Part part2={part2} exercises2={exercises2}/>
+        <Part part3={part3} exercises3={exercises3}/>
+      </div>
     )
   }
 
   const Total = (props) => {
     return (
-      <>
+      <div>
         <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-      </>
+      </div>
     )
   }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3}
-        exercises1={exercises1} exercises2={exercises2}
-        exercises3={exercises3} />
+      <Content/>
       
       <Total exercises1={exercises1} exercises2={exercises2}
         exercises3={exercises3}/>

4af2688f309cf2bb78b63e42b323abc40647aaf5
Leo Tamminen <lettam@utu.fi>
2023-01-24 16:41:06 +0200

    olioita

--- osa1/teht1.4/src/App.js
+++ osa1/teht1.4/src/App.js
@@ -1,17 +1,19 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   const Header = (props) => {
     return (
@@ -21,20 +23,20 @@ const App = () => {
     )
   }
 
-  const Content = ({part1, part2, part3}) => {
+  const Content = ({parts}) => {
     return (
       <div>
-        <p>{part1.name} {part1.exercises}</p>
-        <p>{part2.name} {part2.exercises}</p>
-        <p>{part3.name} {part3.exercises}</p>
+        <p>{parts[0].name} {parts[0].exercises}</p>
+        <p>{parts[1].name} {parts[1].exercises}</p>
+        <p>{parts[2].name} {parts[2].exercises}</p>
       </div>
     )
   }
 
-  const Total = ({part1, part2, part3}) => {
+  const Total = ({parts}) => {
     return (
       <div>
-        <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+        <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
       </div>
     )
   }
@@ -42,11 +44,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2}
-        part3={part3} />
-
-      <Total part1={part1} part2={part2}
-        part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }

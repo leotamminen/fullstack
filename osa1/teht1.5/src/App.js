461ffe54ebbc7e4b66fb88115ffb23687854a6e7
Leo Tamminen <lettam@utu.fi>
2023-01-24 16:53:49 +0200

    muokattu

--- osa1/teht1.5/src/App.js
+++ osa1/teht1.5/src/App.js
@@ -1,24 +1,27 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  const Header = (props) => {
+
+  const Header = ({course}) => {
     return (
       <div>
-        <h1>{props.course}</h1>
+        <h1>{course.name}</h1>
       </div>
     )
   }
@@ -43,9 +46,9 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course = {course} />
+      <Content parts = {course.parts} />
+      <Total parts = {course.parts} />
     </div>
   )
 }

ea77a6dd1f0084f664eaeecbef316c4e0b0443af
Leo Tamminen <lettam@utu.fi>
2023-03-08 16:07:45 +0200

    step6

---
+++ osa2/kurssitiedot/teht2.2/src/App.js
@@ -0,0 +1,60 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+
+
+  const Header = ({course}) => {
+    return (
+      <div>
+        <h1>{course.name}</h1>
+      </div>
+    )
+  }
+
+  const Content = ({parts}) => {
+    return (
+      <div>
+        <p>{parts[0].name} {parts[0].exercises}</p>
+        <p>{parts[1].name} {parts[1].exercises}</p>
+        <p>{parts[2].name} {parts[2].exercises}</p>
+      </div>
+    )
+  }
+
+
+export default App

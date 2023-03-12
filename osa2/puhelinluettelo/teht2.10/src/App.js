a963ef7135d5b8daaaefe6e25fc3135142530ae1
Leo Tamminen <lettam@utu.fi>
2023-03-12 17:17:13 +0200

    2.10

---
+++ osa2/puhelinluettelo/teht2.10/src/App.js
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import Persons from './components/Persons'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' , number: '040-1231244'},
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [filterValue, setFilterValue] = useState('')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const filterPersons = filterValue.length > 0
+    ? persons.filter(person => person.name.toLowerCase().includes(filterValue.toLowerCase()) === true)
+    : persons
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    }
+    else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilterValue(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter filterValue={filterValue} handleFilterChange={handleFilterChange} />
+
+      <h2>add a new</h2>
+
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange}
+      newNumber={newNumber} handleNumberChange={handleNumberChange}/>
+
+      <h2>Numbers</h2>
+      
+      <Persons filterPersons = {filterPersons}/>
+    </div>
+  )
+}
+
+export default App

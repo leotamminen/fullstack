034515946289b0d2a58c39d028fb0333754ecc2a
Leo Tamminen <lettam@utu.fi>
2023-03-12 19:50:01 +0200

    2.12

--- osa2/puhelinluettelo/teht2.12/src/App.js
+++ osa2/puhelinluettelo/teht2.12/src/App.js
@@ -5,12 +5,8 @@ import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , number: '040-1231244'},
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([])
+  
   const [filterValue, setFilterValue] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -25,9 +21,7 @@ const App = () => {
         .get('http://localhost:3001/persons')
         .then(response => {
           console.log('promise fulfilled')
-          setPersons(notes.concat(response.data))
-          setNewName('')
-          setNewNumber('')
+          setPersons(response.data)
         })
     }
     
@@ -44,9 +38,14 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     }
     else {
-      setPersons(persons.concat(personObject))
+      axios
+    .post('http://localhost:3001/persons', personObject)
+    .then(response => {
+      console.log(response)
+      setPersons(persons.concat(response.data))
       setNewName('')
       setNewNumber('')
+    })
     }
   }
 
